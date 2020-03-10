23c3bb837fc2a0ff2daf636c63923a12ca40f43f
dabreadman (YI XIANG TAN) <43485369+dabreadman@users.noreply.github.com>
2020-03-10 11:21:02 +0000

    Add assignment schema/model

---
+++ db/models/assignment-model.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const assignmentSchema = new Schema(
+    {
+		//Assignment
+		title: { type: String, required: true },
+	  description: { type: String, required: true},
+		id: { type: Number, required: true},
+		attachments: { type: [binData], required: true},	
+		first_Start : { type: Date, required: true},
+		first_End : { type: Date, required: true},
+		final_Start : { type: Date, required: true},
+		final_End : { type: Date, required: true},
+		customer: { type: [Number], required: true},
+		
+		//Author 
+		author: { type: Number, required: true},
+		authorName: { type: String, required: true},
+		year: {type: Number, required: true},
+		
+		//Review
+		review_Count: { type: Number, required: true},
+		old_Weight: { type: Number, required: true},
+		references: { type: [binData], required: true},
+		references_Score: { type: Number, required:true},
+		marking_Scheme: { type: binData, required: true},
+		marking_Comments: { type: String, required: true},
+    },
+    { timestamps: true },
+)
+module.exports = mongoose.model('Assignment', assignmentSchema);
