d4abdd64583f92f1a019a512e19fdb921e230edd
dabreadman (YI XIANG TAN) <43485369+dabreadman@users.noreply.github.com>
2020-03-13 10:18:50 +0000

    Update with Assignment

--- db/routes/user-handler.js
+++ db/routes/user-handler.js
@@ -1,13 +1,16 @@
 const path = require('path')
 
 const User = require(path.join(__dirname, '..', 'models', 'user-model.js'))
-const createNewUser = async ( userName, Id, callback
+const createNewUser = async ( userName, Id, Pass, Modules, Role, callback
 
     ) => {
       try {    
         const user = new User({
           name: userName,
           id: Id,
+          pass: Pass,
+          modules: Modules,
+          role: Role,
         });
         user
           .save()
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose');
 const createNewUser = require('./db/routes/user-handler.js')
+const createNewAssignment = require('./db/routes/assignment-handler.js')
 
 const API_PORT = 3001;
 const app = express();
@@ -35,8 +36,6 @@ router.post('/user', cors(), (req, res) => {
     const pass = req.query.pass;
     const modules = req.query.modules;
     const role = req.query.role;
-    console.log(userName);
-    console.log(id);
     createNewUser(
         userName,
         id,
@@ -51,3 +50,50 @@ router.post('/user', cors(), (req, res) => {
       );
 });
 
+router.post('/assignment', cors(), (req, res) => {
+    const title : req.query.title;
+    const description : req.query.description;
+    const id : req.query.id;
+    const attachments : req.query.attachments;
+    const first_start : req.query.first_start;
+    const first_end : req.query.first_end;
+    const final_start : req.query.final_start;
+    const final_end : req.query.final_end;
+    const customer : req.query.customer;
+    const author : req.query.author;
+    const author_name : req.query.author_name;
+    const year : req.query.year;
+    const review_count : req.query.review_count;
+    const old_weight : req.query.old_weight;
+    const references : req.query.references;
+    const references_score : req.query.references_score;
+    const marking_scheme : req.query.marking_scheme;
+    const marking_comments : req.query.marking_comments;
+    createNewAssignment(
+        title,
+        description,
+        id,
+        attachments,
+        first_Start_Start,
+        first_End_End,
+        final_Start_Start,
+        final_End_End,
+        customer,
+        author,
+        author_Name_Name,
+        year,
+        review_Count_count,
+        old_Weight_Weight,
+        references,
+        references_Score_Score,
+        marking_Scheme_Scheme,
+        marking_Comments_Comments, => {
+          return res.json({ data, success: true });
+        },
+        () => {
+          return res.json({
+            success: false,
+          });
+        },
+      );
+});
